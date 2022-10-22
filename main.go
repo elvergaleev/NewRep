c1ea441fa11f492bc3949f88feca7d55c73709bb
Elver Galeev <elvergaleev@gmail.com>
2022-10-22 17:52:01 +0300

    Add FuncCalc In FuncMain

---
+++ main.go
@@ -0,0 +1,14 @@
+Package main
+
+func main () {
+import "fmt"
+
+first:=2
+second:=45
+
+fmt.Println (calc(first, second, sumFunc)
+
+sumFunc:=func (x, y int) int {return x+y)
+}
+
+func calc (x, y, action func (x, y int) int {return action (x, y)}
